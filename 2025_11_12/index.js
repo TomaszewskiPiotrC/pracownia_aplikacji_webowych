9649c9eb405bdb534837c6620e1a2ffef4922907
Piotr Tomaszewski <piotr.tomaszewski@uczen.zsk.poznan.pl>
2025-11-19 08:24:10 +0100

    Delete 2025_11_12 directory

--- 2025_11_12/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-const app = express()
-
-app.listen(3000, () => {
-  console.log('App is running on https://localhost:3000')
-})
