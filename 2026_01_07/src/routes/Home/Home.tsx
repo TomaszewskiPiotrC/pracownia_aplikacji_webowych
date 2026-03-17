3be59a1f0eb3e9cf418bdeeeaa71c8a731efa4d4
Piotr Tomaszewski <piotr.tomaszewski@uczen.zsk.poznan.pl>
2026-03-17 23:00:49 +0100

    Add files via upload

--- 2026_01_07/src/routes/Home/Home.tsx
+++ 2026_01_07/src/routes/Home/Home.tsx
@@ -3,7 +3,7 @@ import styles from "./Home.module.scss"
 export default function Home() {
     return (
         <nav className={styles.Home}>
-            <h2>Witaj na stronie głównej blogu!</h2>
+            <h2>Witaj na stronie głównej aplikacji bloga!</h2>
         </nav>
     )
 }
